7abb2646237ee6002f5dd36a3a4dc04b592a77b3
madashivakarthikgoud <madashivakarthikgoud@gmail.com>
2025-08-15 21:32:29 +0530

    webhooks added

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import cors from 'cors'
 import { connect } from 'mongoose'
 import connectDB from './config/mongodb.js'
+import userRouter from './routes/userRoutes.js'
 
 const PORT = process.env.PORT  || 4000
 const app=express()
@@ -12,5 +13,6 @@ app.use(express.json())
 app.use(cors())
 
 app.get('/',(req,res)=>res.send("API Working"))
+app.use('/api/user',userRouter)
 
 app.listen(PORT,()=>console.log("Server Running on port : "+PORT))
